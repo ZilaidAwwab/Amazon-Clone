4e4b5d9e3290317ac80896bf7979f703ea23663c
ZilaidAwwab <zilaidawwab786@gmail.com>
2024-03-12 02:18:53 +0500

    config settings

---
+++ src/Components/Home.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <div className="home_container">
+        <img
+          className="home_image"
+          src="src\assets\images\hero_image.jpg"
+          alt=""
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDbJtV9njRtIQlj4JedzLmYUdDxS0nsPDk",
+  authDomain: "clone-83995.firebaseapp.com",
+  projectId: "clone-83995",
+  storageBucket: "clone-83995.appspot.com",
+  messagingSenderId: "123192770600",
+  appId: "1:123192770600:web:28aa2f47226053415ef51d",
+  measurementId: "G-HKQQN0R9DY",
+};
